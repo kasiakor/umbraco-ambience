57881909cf64c302db14f110478537615d4c09d2
kasia.kordys <kasia.kordys@gmail.com>
2020-07-03 23:31:19 +0100

    add contact form js file

---
+++ UmbracoAmbience.Web/scripts/contactForm.js
@@ -0,0 +1,19 @@
+﻿var contactForm = contactForm || {
+
+    init: function () {
+        this.listeners();
+    },
+
+    listeners: function () {
+        $(document).on("click", ".contact-submit", function (e) {
+            e.preventDefault();
+            var form = $(this).closest("form");
+            form.submit();
+        }),
+
+    showResult: function() {
+        $("#form-outer").hide();
+        $("#form-result").show();
+    }
+    }
+contactForm.init()
