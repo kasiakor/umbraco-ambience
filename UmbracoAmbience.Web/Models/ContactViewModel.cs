816e25d21ea6a57a77089bf7edb6f9a5269aac26
kasia.kordys <kasia.kordys@gmail.com>
2020-07-03 20:18:31 +0100

    add contact view model

---
+++ UmbracoAmbience.Web/Models/ContactViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UmbracoAmbience.Web.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage ="Please enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="Please enter your email address")]
+        [EmailAddress(ErrorMessage ="Please enter valid email address")]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(500, ErrorMessage ="Your message must be less that 500 characters")]
+        public string Message { get; set; }
+    }
+}
