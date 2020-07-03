4e8782429a451923c70cc037c15ed20270c2563f
kasia.kordys <kasia.kordys@gmail.com>
2020-07-03 22:09:36 +0100

    add action methods to contact controller, add success/error partial view

---
+++ UmbracoAmbience.Web/Controllers/ContactController.cs
@@ -0,0 +1,44 @@
+﻿using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using UmbracoAmbience.Web.Models;
+
+namespace UmbracoAmbience.Web.Controllers
+{
+    public class ContactController : SurfaceController
+    {
+        public string GetPathView(string name)
+        {
+            return $"/Views/Partals/Content/{name}.cshtml";
+        }
+
+        [HttpGet]
+        public ActionResult RenderForm()
+        {
+            ContactViewModel model = new ContactViewModel();
+            return PartialView(GetPathView("_ContactForm"), model);
+        }
+
+        [HttpPost]
+        public ActionResult RenderForm(ContactViewModel model)
+        {
+            return PartialView(GetPathView("_ContactForm"), model);
+        }
+
+        [HttpPost]
+        public ActionResult SubmitForm(ContactViewModel model)
+        {
+            bool success = false;
+            if (ModelState.IsValid)
+            {
+                success = SendEmail(model);
+            }
+            return PartialView(GetPathView(success ? "_Success" : "_Error"));   
+        }
+
+        public bool SendEmail(ContactViewModel model)
+        {
+            //GetHashCode for sending an email
+            return true;
+        }
+    }
+}
