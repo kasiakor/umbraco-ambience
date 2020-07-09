ab7483627dc5468c8a3a1eaaf920305807f536cc
kasia.kordys <kasia.kordys@gmail.com>
2020-07-09 21:40:05 +0100

    add news article model and news article list model

---
+++ UmbracoAmbience.Web/Models/NewsArticleListModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UmbracoAmbience.Web.Models;
+
+namespace UmbracoAmbience.Web.Models
+{
+    public class NewsArticleListModel
+    {
+        public List<NewsArticleModel> Items { get; set; }
+        public bool HasItems => Items != null && Items.Count > 0;
+        public int ItemsCount => HasItems ? Items.Count : 0;
+    }
+}
---
+++ UmbracoAmbience.Web/Models/NewsArticleModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UmbracoAmbience.Web.Models
+{
+    public class NewsArticleModel 
+    {
+        public string Title { get; set; }
+        public string Intro { get; set; }
+        public string Url { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}
